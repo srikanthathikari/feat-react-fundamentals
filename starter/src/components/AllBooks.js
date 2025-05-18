e48b52e1724d0ef43f99d90f9100f871c67dabcc
Srikanth Athikari <srikantathikari@gmail.com>
2025-05-18 16:50:56 -0400

    Revert "avoid duplication and fix shelfing."
    This reverts commit e730892029e34376e750cca99181363cbc29adc8.

--- starter/src/components/AllBooks.js
+++ starter/src/components/AllBooks.js
@@ -1,65 +1,62 @@
 import { useState } from "react";
 
 const AllBooks = ({ bookData, updateData, removeData }) => {
-  const onChangeHandler = (event, bookItem, currentShelf) => {
-    const newShelf = event.target.value;
+  const onChangeHandler = (event, bookItem, book) => {
+    const whereToAdd = event.target.value;
+    const whatArray = bookItem;
+    const whereToRemove = book;
 
-    if (newShelf === currentShelf) {
-      // If the shelf hasn't changed, do nothing
-      return;
-    }
-
-    // Remove the book from the current shelf
-    removeData(currentShelf, bookItem);
-
-    // Add the book to the new shelf
-    updateData(newShelf, bookItem);
+    removeData(whereToRemove, whatArray);
+    updateData(whereToAdd, whatArray);
   };
-
   return (
     <div>
-      {Object.keys(bookData).map((shelf) => (
-        <div key={shelf}>
-          <h2 className="bookshelf-title">{shelf}</h2>
+      {Object.keys(bookData).map((book) => (
+        <div key={book}>
+          <h2 className="bookshelf-title">{book}</h2>
           <div className="bookshelf-books">
             <ol className="books-grid">
-              {bookData[shelf].map((bookItem, index) => (
-                <li key={index}>
-                  <div className="book">
-                    <div className="book-top">
-                      <div
-                        className="book-cover"
-                        style={{
-                          width: 128,
-                          height: 193,
-                          backgroundImage: `url("${
-                            bookItem.url || bookItem.imageLinks?.smallThumbnail || ""
-                          }")`,
-                        }}
-                      ></div>
-                      <div className="book-shelf-changer">
-                        <select
-                          value={shelf} // Set the current shelf as the selected value
-                          onChange={(event) =>
-                            onChangeHandler(event, bookItem, shelf)
-                          }
-                        >
-                          <option value="Currently Reading">
-                            Currently Reading
-                          </option>
-                          <option value="Want to read">Want to read</option>
-                          <option value="Read">Read</option>
-                          <option value="none">none</option>
-                        </select>
+              {bookData[book].map(
+                (bookItem, index) => (
+                  // console.log(bookItem),
+                  (
+                    <li key={index}>
+                      <div className="book">
+                        <div className="book-top">
+                          <div
+                            className="book-cover"
+                            style={{
+                              width: 128,
+                              height: 193,
+                              backgroundImage: `url("${
+                                bookItem.url ||
+                                bookItem.imageLinks.smallThumbnail
+                              })`,
+                            }}
+                          ></div>
+                          <div className="book-shelf-changer">
+                            <select
+                              value={book}
+                              onChange={(event) =>
+                                onChangeHandler(event, bookItem, book)
+                              }
+                            >
+                              <option value="Currently Reading">
+                                Currently Reading
+                              </option>
+                              <option value="Want to read">Want to read</option>
+                              <option value="Read">Read</option>
+                              <option value="none">none</option>
+                            </select>
+                          </div>
+                        </div>
+                        <div className="book-title">{bookItem.title}</div>
+                        <div className="book-authors">{bookItem.author || bookItem["authors"]}</div>
                       </div>
-                    </div>
-                    <div className="book-title">{bookItem.title}</div>
-                    <div className="book-authors">
-                      {bookItem.author || bookItem.authors?.join(", ")}
-                    </div>
-                  </div>
-                </li>
-              ))}
+                    </li>
+                  )
+                )
+              )}
             </ol>
           </div>
         </div>
